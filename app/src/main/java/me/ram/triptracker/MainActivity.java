5b2e4ad876aefa7d172a4cceeea03cf5e440154a
Ram Surya <thegreatwarriorking3@gmail.com>
2024-11-09 11:00:34 +0530

    second commit

--- app/src/main/java/me/ram/triptracker/MainActivity.java
+++ app/src/main/java/me/ram/triptracker/MainActivity.java
@@ -11,18 +11,25 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private
     DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    Expense expense;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,27 +91,51 @@ public class MainActivity extends AppCompatActivity {
                 String date = dateInput.getText().toString();
 
                 // Create a new expense object
-                Expense expense = new Expense(expenseName, amount, category, date);
-
+                //Expense expense = new Expense(expenseName, amount, category, date);
+                addDatatoFirebase(expenseName, amount, category, date);
                 // Save expense to Firebase
-                String expenseId = databaseReference.push().getKey();
-                if (expenseId != null) {
-                    databaseReference.child(expenseId).setValue(expense)
-                            .addOnCompleteListener(task -> {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(MainActivity.this, "Expense saved", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(MainActivity.this, "Failed to save expense", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                }
+                //String expenseId = databaseReference.push().getKey();
+
 
                 // Dismiss dialog after saving
                 dialog.dismiss();
             }
         });
 
+
+
         // Show the dialog
         dialog.show();
+    }
+    private void addDatatoFirebase(String name, Double amount, String category, String date) {
+        // below 3 lines of code is used to set
+        // data in our object class.
+        expense.setName(name);
+        expense.setAmount(amount);
+        expense.setCategory(category);
+        expense.setDate(date);
+
+
+        // we are use add value event listener method
+        // which is called with database reference.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // inside the method of on Data change we are setting
+                // our object class to our database reference.
+                // data base reference will sends data to firebase.
+                databaseReference.setValue(expense);
+
+                // after adding this data we are showing toast message.
+                Toast.makeText(MainActivity.this, "data added", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // if the data is not added or it is cancelled then
+                // we are displaying a failure toast message.
+                Toast.makeText(MainActivity.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
